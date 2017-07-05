211767d8c4b67c55e82180939a40d74cbe34b181
Roni Choudhury <roni.choudhury@kitware.com>
2017-07-05 14:16:03 -0400

    Add some comments

--- src/index.js
+++ src/index.js
@@ -13,14 +13,19 @@ import './index.less';
 import './index.styl';
 import content from './index.jade';
 
+// Install the main page content.
 select(document.body).html(content());
 
+// Convert the raw data into JSON records.
 const data = dataRaw.split('\n')
   .filter(x => x.length > 0)
   .map(x => JSON.parse(x));
 
+// Kick off the application by installing the data to the store.
 store.dispatch(action.setDataStream(data));
 
+// Install action handlers
+//
 // Play button.
 select('#play').on('click', () => {
   store.dispatch(action.start());
@@ -46,19 +51,27 @@ select('#faster').on('click', () => {
   store.dispatch(action.increaseSpeed());
 });
 
+// Install reactive actions to changes in the store.
+//
+// When the data pointer changes.
 observeStore(next => {
   const index = next.getIn(['datastream', 'index']);
   console.log(index);
 
+  // Disable the rewind button whenever playback is at the very start of the
+  // data.
   select('#rewind').attr('disabled', index === 0 ? true : null);
 }, s => s.getIn(['datastream', 'index']));
 
+// When the playback speed changes.
 observeStore(next => {
   const interval = next.getIn(['playback', 'interval']);
   if (next.getIn(['playback', 'running'])) {
     start(interval);
   }
 
+  // Disable the speed change buttons if the speed reaches the ends of the
+  // range.
   if (interval === next.getIn(['playback', 'intervalHigh'])) {
     select('#slower').attr('disabled', true);
   } else if (interval === next.getIn(['playback', 'intervalLow'])) {
@@ -69,15 +82,18 @@ observeStore(next => {
   }
 }, s => s.getIn(['playback', 'interval']));
 
+// When the playback state changes.
 observeStore(next => {
   const running = next.getIn(['playback', 'running']);
 
+  // Start/stop playback as needed.
   if (running) {
     start(next.getIn(['playback', 'interval']));
   } else {
     stop();
   }
 
+  // Disable the play/stop buttons to reflect the playback state.
   select('#play').attr('disabled', running ? true : null);
   select('#stop').attr('disabled', running ? null : true);
 }, s => s.getIn(['playback', 'running']));
