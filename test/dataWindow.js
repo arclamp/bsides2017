133bc5c0cc836126a303c19f3c399b206d9e0faf
Roni Choudhury <roni.choudhury@kitware.com>
2017-07-18 10:51:40 -0400

    Add TDD tests for SliceWindow

--- test/dataWindow.js
+++ test/dataWindow.js
@@ -42,3 +42,69 @@ test('Initialized DataWindow behavior', t => {
 
   t.end();
 });
+
+test('SliceWindow default behavior', t => {
+  let dw = new DataWindow({
+    size: 100
+  });
+
+  let slice1 = new SliceWindow({
+    size: 10,
+    dataWindow: dw
+  });
+
+  const data = [...Array(100).keys()];
+  const ten = data.slice(0, 10);
+  ten.forEach(d => dw.add(d));
+
+  t.deepEqual(dw.data, ten, 'DataWindow has 0..9');
+  t.deepEqual(slice1.data, ten, 'SliceWindow has 0..9');
+
+  data.slice(10).forEach(d => dw.add(d));
+
+  t.deepEqual(dw.data, data, 'DataWindow has 0..99');
+  t.deepEqual(slice1.data, data.slice(-10), 'SliceWindow has 90..99');
+
+  t.end();
+});
+
+test('SliceWindow fixed behavior', t => {
+  let dw = new DataWindow({
+    size: 100
+  });
+
+  let slice1 = new SliceWindow({
+    start: 40,
+    size: 10,
+    dataWindow: dw
+  });
+
+  const data = [...Array(100).keys()];
+  const forty = data.slice(0, 40);
+  forty.forEach(d => dw.add(d));
+
+  t.deepEqual(dw.data, forty, 'DataWindow has 0..39');
+  t.deepEqual(slice1.data, forty.slice(-10), 'SliceWindow has 30..39');
+
+  const toFifty = data.slice(40, 50);
+  toFifty.forEach(d => dw.add(d));
+
+  t.deepEqual(dw.data, data.slice(0, 50), 'DataWindow has 0..49');
+  t.deepEqual(slice1.data, toFifty, 'SliceWindow has 40..49');
+
+  dw.add(50);
+  t.deepEqual(dw.data, data.slice(0, 51), 'DataWindow has 0..50');
+  t.deepEqual(slice1.data, toFifty, 'SliceWindow still has 40..49');
+
+  const rest = data.slice(51);
+  rest.forEach(d => dw.add(d));
+
+  t.deepEqual(dw.data, data, 'Full DataWindow has 0..99');
+  t.deepEqual(slice1.data, toFifty, 'SliceWindow still has 40..49');
+
+  dw.add(100);
+  t.deepEqual(dw.data, data.map(x => x + 1), 'Overfull DataWindow has 1..100');
+  t.deepEqual(slice1.data, toFifty.map(x => x + 1), 'SliceWindow has 41..50');
+
+  t.end();
+});
